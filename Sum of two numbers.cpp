d560e9a9000fcd6e7a006e14d4b6134f8e6e79af
Rakesh Kumar <76479858+Rakeshkumar-321@users.noreply.github.com>
2022-11-27 17:43:35 +0530

    Create Sum of two numbers.cpp

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // << endl or '/n'; done se next line or new line me jata hai.
+
+  int a, b, sum ;
+  a=5;
+  b=10;
+  sum=a+b;
+  cout<<"\n";
+  cout<<"Addition of two numbers" << sum;
+  cout<<"Addition of two numbers" << + sum;
+  }
