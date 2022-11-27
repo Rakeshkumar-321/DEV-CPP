6897ea2d30c98e64b54d4c2f6c1463df4122fa21
Rakesh Kumar <76479858+Rakeshkumar-321@users.noreply.github.com>
2022-11-27 17:43:22 +0530

    Create Less & Greater than a&b.cpp

---
+++ Less & Greater than a&b.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Less than a<b
+
+int main() {
+  // << endl or '/n'; done se next line or new line me jata hai.
+  int a, b;
+  a = 12;
+  b = 11;
+
+  if (a < b)
+
+  {
+    cout << " YES A is less then B " << endl;
+  } else {
+    cout << " NO A is not less then B " << endl;
+  }
+}
+
+// Greater than a>b
+int main() {
+  // << endl or '/n'; done se next line or new line me jata hai.
+  int a, b;
+  a = 12;
+  b = 11;
+
+  if (a < b)
+
+  {
+    cout << " YES A is less then B " << endl;
+  } else {
+    cout << " NO A is not less then B " << endl;
+  }
+}
