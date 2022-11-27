e4cc0218b9968ed45fd2cfc41796d710b5f1ceb3
Rakesh Kumar <76479858+Rakeshkumar-321@users.noreply.github.com>
2022-11-27 17:35:39 +0530

    Create Avg of 3 Numbers.cpp

---
+++ Avg of 3 Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // << endl or '/n'; done se next line or new line me jata hai.
+
+  int a,b,c,avg;
+  a=5;
+  b=8;
+  c=15;
+  avg=(a+b+c) / 3;
+cout<<"Average of 3 Numbers = "<<avg<<endl;
+}
