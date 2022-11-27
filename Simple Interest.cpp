5af92c71edc77a4a6fd02ebf3c2b95596f88641a
Rakesh Kumar <76479858+Rakeshkumar-321@users.noreply.github.com>
2022-11-27 17:43:39 +0530

    Create Simple Interest.cpp

---
+++ Simple Interest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // << endl or '/n'; done se next line or new line me jata hai.
+
+  int p, r, t, si;
+  p = 1000;
+  r = 5;
+  t = 3;
+  si = p * r * t / 100;
+  cout << "Simple Interest = " << si << endl;
+}
